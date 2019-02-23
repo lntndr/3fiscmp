56b8c403736ca9d47af5bb43d1179350fac9ec6e
Andrea Lanterna <a.lanterna@campus.unimib.it>
2019-02-23 23:12:38 +0100

    Completa prima domanda moler_2_15

--- moler_2_15/moler_2_15.m
+++ moler_2_15/moler_2_15.m
@@ -1,7 +1,7 @@
 function moler_2_15
 
 % Variabili modificabili a piacere                    
-max_order=4;            % Ogni tentativo viene eseguito con su
+max_order=2;            % Ogni tentativo viene eseguito con su
                         % una matrice n*n con n=m*10^(order), m definito
                         % da un ciclo for. Il programma dovrebbe gestire 
                         % eccezioni dovute al sopraggiungere del limite di
@@ -13,11 +13,14 @@ smt_wrong=0;
 order=0;
 
 hold on
+
+m_x=zeros(5*(max_order+1),1);
+m_y=m_x;
                         
 while (smt_wrong==0) && (order <= max_order)
     tries=get_tries(order);
-    for m=0:2:8
-        x=zeros(tries,1);   % 3 tentativi (3,6,9) per ordine da 0 a salire
+    for m=2:2:10
+        x=zeros(tries,1);   
         y=x;
         try
             for n=1:tries 
@@ -28,16 +31,20 @@ while (smt_wrong==0) && (order <= max_order)
             smt_wrong=1;
             break;
         end
-        scatter(x,y,2,'filled');
+        scatter(x,y,2,[0.8, 0.8, 0.8],'filled');
+        m_x(5*order+m/2)=m*10^(order);
+        m_y(5*order+m/2)=sum(y)/tries;
     end
     order=order+1;
 end
+
+plot(m_x,m_y,'*k');
 
 set(gca, 'YScale', 'log')
 set(gca, 'XScale', 'log')
 
 function x=get_tries(order)
-if  10^(3-order)>10
+if  10^(3-order)>=10
     x=10^(3-order);
 else
     x=5;
