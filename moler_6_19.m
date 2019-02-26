6191edcce607d059d30c97f3a7e9110090cfb6d0
lntndr <47917882+lntndr@users.noreply.github.com>
2019-02-26 17:17:13 +0100

    Aggiorna con grafico moler_6_19
    Il grafico non è strettamente richiesto dall'esercizio ma aiuta a ragionare sulla parte due ancora da fare

--- moler_6_19.m
+++ moler_6_19.m
@@ -2,19 +2,31 @@ function moler_6_19
 %Domanda a
 %0 è punto di accumulazione degli zeri della 
 
-k_max=20;
-x_k=(zeros(k_max+1,1));
+k_max=750;              %Valori maggiori richiedono modelli più sofisticati
+x_k=(ones(k_max+1,1));
 
 for k=1:k_max;
-f=@(x) log(x)/x+pi*(k-1/2)
-x_k(k+1)=fzero(f,1/k) %1/k serie decrescente più facile che mi è venuta in
-                    %mente, niente di più raffinato
+f=@(x) log(x)/x+pi*(k-1/2);
+x_k(k+1)=fzero(f,1/k); %1/k serie decrescente più facile che mi è venuta in
+                       %mente, niente di più raffinato
 end
 
-f=@(x) x^-1*cos(x^(-1)*log(x));
-
-for k=1:k
-    
-    
-
+g=@(t) cos(log(t)./t)./t ;
+s=0;
+s_d=0;
+hold on
+s_dv=zeros(k_max/2,1);
+s_pv=s_dv;
+for k=1:k_max
+    if mod(k,2)~=0
+        s_d=s+quad(g,x_k(k+1),x_k(k));
+        s_dv((k+1)/2)=s_d;
+    else
+        s=s_d+quad(g,x_k(k+1),x_k(k));
+        s_pv(k/2)=s;
+    end
 end
+plot(1:2:k,s_dv,'.k');
+plot(2:2:k,s_pv,'.r');
+m=(s_dv+s_pv)./2;
+plot(1.5:2:k+0.5,m,'.b');
