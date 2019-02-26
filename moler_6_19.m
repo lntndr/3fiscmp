ca6bde69974a66811be2e375a385d64c452180e7
Andrea Lanterna <a.lanterna@campus.unimib.it>
2019-02-26 22:30:32 +0100

    Rinnova motore degli zeri

--- moler_6_19.m
+++ moler_6_19.m
@@ -1,62 +1,59 @@
 function moler_6_19
 %Domanda a
-%0 è punto di accumulazione degli zeri della 
-%Ciclo while sufficientemente complesso
-%   A. Un while cerca zeri al crescere di k cercandoli al decrescere di
-%           a+b/x
-%   B. Se ha trovato più zeri della volta prima crea un vettore
-%   C. Un ciclo for popola il vettore con i nuovi zeri
-%   D. Un fit degli zeri ridefinisce a e b facendo ricomiciare il ciclo
-% Il programma fitta solo una delle due sottosequenze, quella positiva
-% Non escludo che ci siano scelte più raffinate
+%0 è punto di accumulazione degli zeri della funzione
+
 k=1;
 c=[0,1];
 new_max=1;
-k=1;
-fun = @(c,xdata) c(1)+(c(2)/xdata);
-x_k=[];
+fit_fun = @(c,x) c(1)+(c(2)/x);
+x_k=ones();
+tic;
 while 1
   %%% PARTE A
-  max=new_max;
+  old_max=new_max;
   while 1
       k=k+1;
-      f=@(x) log(x)/x+pi*(k-1/2);
-      prk=fzero(f,c(1)+(c(2)/k));
+      zero_fun=@(x) log(x)/x+pi*(k-1/2);
+      prk=fzero(zero_fun,c(1)+(c(2)/k));
       if isnan(prk)
           new_max=k-1;
           disp("Impossibile trovare zeri ulteriori");
           break;
+      else
+          if k==755
+              disp("UÈ NANI!");
+          end
+          x_k(k)=prk;
       end
   end
+  b=toc;
   %%% PARTE B
-  if new_max>max
-      x_k=[x_k,k+1:new_max];
-      %%% PARTE C
-      for n=1:new_max;
-        f=@(x) log(x)/x+pi*(n-1/2);
-        x_k(n)=fzero(f,c(1)+(c(2)/n));
-      end
-      %%% PARTE D
-      disp('DDD');
-      c = lsqcurvefit(fun,1,1:new_max,x_k);
+  if new_max>old_max && b < 1
+      c=lsqcurvefit(fit_fun,c,(1:new_max)',x_k);
   else
       break; %<<<----- !Il ciclo finisce qui!
   end
+  
 end
 
 g=@(t) cos(log(t)./t)./t ;
 s=0;
 s_d=0;
 hold on
-s_dv=zeros(k_max/2,1);
-s_pv=s_dv;
-for k=1:k_max
-    if mod(k,2)~=0
-        s_d=s+quad(g,x_k(k+1),x_k(k));
-        s_dv((k+1)/2)=s_d;
+if mod(new_max,2)==0
+    s_dv=zeros(new_max/2,1);
+    s_pv=s_dv;
+else
+    s_dv=zeros(floor(new_max/2),1);
+    s_dv=zeros(ceil(new_max/2),1);
+end
+for n=1:new_max
+    if mod(n,2)~=0
+        s_d=s+integral(g,x_k(n+1),x_k(n));
+        s_dv((n+1)/2)=s_d;
     else
-        s=s_d+quad(g,x_k(k+1),x_k(k));
-        s_pv(k/2)=s;
+        s=s_d+integral(g,x_k(n+1),x_k(n));
+        s_pv(n/2)=s;
     end
 end
 plot(1:2:k,s_dv,'.k');
