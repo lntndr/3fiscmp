0ee68264510866099ec6b9619dcac82e7b15b013
Andrea Lanterna <a.lanterna@campus.unimib.it>
2019-02-27 11:56:07 +0100

    Migliora motore fit

--- moler_6_19.m
+++ moler_6_19.m
@@ -7,6 +7,8 @@ c=[0,1];
 new_max=1;
 fit_fun = @(c,x) c(1)+(c(2)/x);
 x_k=ones();
+%time=10;
+
 tic;
 while 1
   %%% PARTE A
@@ -14,51 +16,73 @@ while 1
   while 1
       k=k+1;
       zero_fun=@(x) log(x)/x+pi*(k-1/2);
-      prk=fzero(zero_fun,c(1)+(c(2)/k));
+      
+      try
+        prk=fzero(zero_fun,c(1)+(c(2)/k));
+      catch
+          
+        disp('Errore allo zero');
+        disp(k);
+        
+        disp('Impossibile cercare zero a partire dal punto');
+        disp(c(1)+(c(2)/k));
+        
+        disp('Risultato precedente');
+        disp(prk);
+        k=k-1;
+        new_max=k;
+        break;  
+      end
+      
       if isnan(prk)
-          new_max=k-1;
-          disp("Impossibile trovare zeri ulteriori");
+          k=k-1;
+          new_max=k;
           break;
       else
-          if k==755
-              load handel;
-              sound(y, Fs);
-              disp("UÈ NANI!");
-          end
           x_k(k)=prk;
       end
   end
-  b=toc;
+  
+  b=toc
+  
   %%% PARTE B
-  if new_max>old_max
-      c=lsqcurvefit(fit_fun,c,(1:new_max)',x_k);
+  if new_max>old_max % && b< time
+      c=lsqcurvefit(fit_fun,c,(1:new_max)',x_k,zeros(size(c)));
   else
       break; %<<<----- !Il ciclo finisce qui!
   end
   
 end
 
+%%% Calcolo deli integrali una volta ottenuta la serie di zeri
+
 g=@(t) cos(log(t)./t)./t ;
 s=0;
 s_d=0;
 hold on
-if mod(new_max,2)==0
-    s_dv=zeros(new_max/2,1);
-    s_pv=s_dv;
-else
-    s_dv=zeros(floor(new_max/2),1);
-    s_dv=zeros(ceil(new_max/2),1);
+if mod(new_max,2)~=0
+    new_max=new_max-1;
 end
-for n=1:new_max
+
+s_dv=ones(new_max/2,1);
+s_pv=s_dv;
+
+for n=2:new_max
     if mod(n,2)~=0
-        s_d=s+integral(g,x_k(n+1),x_k(n));
+        s_d=s+integral(g,x_k(n),x_k(n-1));
         s_dv((n+1)/2)=s_d;
     else
-        s=s_d+integral(g,x_k(n+1),x_k(n));
+        s=s_d+integral(g,x_k(n),x_k(n-1));
         s_pv(n/2)=s;
     end
 end
-plot(1:2:k,s_dv,'.k');
-plot(2:2:k,s_pv,'.r');
+
 m=(s_dv+s_pv)./2;
-plot(1.5:2:k+0.5,m,'.b');
+
+disp('Penultima e ultima media');
+disp(m((new_max/2)-1));
+disp(m((new_max/2)));
+
+plot(1:2:new_max,s_dv,'.k');
+plot(2:2:new_max,s_pv,'.r');
+plot(1.5:2:new_max+0.5,m,'.b');
