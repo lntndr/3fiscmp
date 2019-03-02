c21a7b5e5f17203665bd16e30201de7a6e9ba1d2
Andrea Lanterna <a.lanterna@campus.unimib.it>
2019-03-02 13:59:21 +0100

    Corregge errori funzione gamma

--- moler_4_11.m
+++ moler_4_11.m
@@ -3,18 +3,18 @@ function moler_4_11
 %Domanda a
 k=0;
 x=0;
-s_x=sym(0);
+s_x=0;
 %Sintassi vagamente cervellotica per sostituire do-while
 while 1
     k=k+1;
     x=factorial(k);
-    s_x=factorial(sym(k));
+    s_x=gamma(k+1);
     if (s_x~=x)
         k=k-1;
         break;
     end
 end
-disp('Il più grande numero che ammette fattoriale esattamente in double');
+disp('Il più grande numero per cui gamma coincide con il fattoriale esattamente in double');
 disp(k);
 
 %Domanda b
@@ -29,4 +29,16 @@ while 1
 end
 disp('Il più grande numero che ammette fattoriale senza owerflow');
 disp(k);
-    
+
+k=0;
+x=0;
+while 1
+    k=k+1;
+    if (gamma(k+1))>=inf
+        k=k-1;
+        break;
+    end
+end
+disp('Il più grande numero che ammette gamma senza owerflow');
+disp(k);
+end
